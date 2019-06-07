970d5c803ffda23feec5d9b1209099a0900c1ce5
Alexander Schelchere <ascellie@gmail.com>
2019-06-07 12:38:13 -0400

    added movies

--- run.rb
+++ run.rb
@@ -16,6 +16,14 @@ phil = Actor.new("Phillip Seymore Hoffman", 2019)
 gabby = Actor.new("Gabbrielle Union", 2001)
 jmo = Actor.new("Janelle Monet", 1933)
 
+avengers = Movie.new("Avengers: Endgame", "Superhero", 3000000)
+blade_runner = Movie.new("Blade Runner", "Science Fiction", 2)
+bring = Movie.new("Bring It On", "Comedy", 3)
+moonlight = Movie.new("Moonlight", "Drama", 20)
+ghost = Movie.new("Ghost in the Shell", "Science Fiction", 20)
+batman = Movie.new("Batman Begins", "Superhero", 400)
+hk = Movie.new("Harold & Kumar", "Comedy", 600)
+
 
 
 # Test with pry
